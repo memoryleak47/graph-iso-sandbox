9335b7eb9df0073b90037241bf70ff3088a16658
memoryleak47 <memoryleak47@protonmail.ch>
2024-07-07 02:37:00 +0200

    ...

--- src/simulation.rs
+++ src/simulation.rs
@@ -10,9 +10,9 @@ pub enum FootprintResult<T> {
     Different(T, T),
 }
 
-pub fn witness_non_isomorphism<S: Simulation>(g1: Graph, g2: Graph) -> FootprintResult<S::Footprint> {
-    let g1 = SymbolGraph::from(&g1);
-    let g2 = SymbolGraph::from(&g2);
+pub fn witness_non_isomorphism<S: Simulation>(g1: &Graph, g2: &Graph) -> FootprintResult<S::Footprint> {
+    let g1 = SymbolGraph::from(g1);
+    let g2 = SymbolGraph::from(g2);
     let f1 = S::simulate(&g1);
     let f2 = S::simulate(&g2);
     if f1 != f2 {
